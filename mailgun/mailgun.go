671d749baec2a52baea77bc15bce349f84746aad
Nizigama jean davy <jedavy.n@gmail.com>
2021-04-06 21:50:27 +0200

    Created the mailgun package

---
+++ mailgun/mailgun.go
@@ -0,0 +1,24 @@
+package mailgun
+
+import (
+	"context"
+	"time"
+
+	"github.com/mailgun/mailgun-go/v3"
+)
+
+type MailgunConfig struct {
+	Domain string `json:"domain"`
+	ApiKey string `json:"apiKey"`
+}
+
+func SendTextMessage(domain, apiKey, sender, subject, body string, recipients []string) (string, string, error) {
+	mg := mailgun.NewMailgun(domain, apiKey)
+	m := mg.NewMessage(sender, subject, body, recipients...)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+
+	statusMessage, messageID, err := mg.Send(ctx, m)
+	return statusMessage, messageID, err
+}
